356e9b6af18dec32d8e3cd3f89fb74f4c51fdda0
Jashvinu Y <72579269+Jashvinu@users.noreply.github.com>
2025-02-19 21:40:39 +0530

    upd

--- app.py
+++ app.py
@@ -34,40 +34,28 @@ with st.sidebar:
 # ----------------------------------- Data Loading ------------------------------
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
-# Read the data
 balance_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
     worksheet="balance_sheet",
 )
-
 income_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
-    worksheet="income_statement",
+    worksheet="income_data",
 )
-
 cash_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
     worksheet="cash_flow"
 )
 customers_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
     worksheet="customers_report"
 )
 sales_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
     worksheet="sales_report"
 )
 products_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
     worksheet="products_data"
 )
 market_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
     worksheet="market_data"
 )
 media_data = conn.read(
-    spreadsheet=st.secrets.gsheets.spreadsheet,  # Add this line
     worksheet="media_data"
 )
 
